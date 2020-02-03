bc56b9a7e080898605cb26c04db3f66444a898d7
Kaustubh Nair <kaustubh.nair108@gmail.com>
2020-02-03 19:08:17 +0530

    Fix fragment shader

--- source/shader.cpp
+++ source/shader.cpp
@@ -47,7 +47,7 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
 
   // similiar for Fragment Shader
   fragment = glCreateShader(GL_FRAGMENT_SHADER);
-  glShaderSource(fragment, 1, &vShaderCode, NULL);
+  glShaderSource(fragment, 1, &fShaderCode, NULL);
   glCompileShader(fragment);
   // print compile errors if any
   glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
--- source/shaders/shader.fs
+++ source/shaders/shader.fs
@@ -1,11 +1,11 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 vertexColor;
+uniform vec4 vertexColor;
 
 void main()
 {
-    FragColor = vec4(vertexColor, 1.0f);
+    FragColor = vertexColor;
 }
 
 
