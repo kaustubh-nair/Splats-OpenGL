df3521544f86b00c4e465d41c8dd578e023dfe1b
Kaustubh Nair <kaustubh.nair108@gmail.com>
2020-02-14 15:15:04 +0530

    lighting yay

--- source/controller.cpp
+++ source/controller.cpp
@@ -4,6 +4,7 @@
 
 void Controller::mainLoop( void )
 {
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
   GLFWwindow* window = this->mainWindow;
 
   glewExperimental = GL_TRUE;
@@ -23,25 +24,19 @@ void Controller::mainLoop( void )
   glEnable(GL_DEPTH_TEST);
   glPolygonMode( GL_FRONT_AND_BACK, GL_FILL );
 
-  glm::mat4 proj = glm::perspective(glm::radians(45.0f), (float)800/(float)600, 0.1f, 100.0f);
-  glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f,0.0f,-20.0f));
 
-  lightingShader.use();
-  lightingShader.setMat4("projection", proj);
-  lightingShader.setMat4("view", view);
-
-  shader.use();
-  shader.setMat4("projection", proj);
 
   while(!glfwWindowShouldClose(window))
   {
+    glm::mat4 proj = glm::perspective(glm::radians(45.0f), (float)800/(float)600, 0.1f, 100.0f);
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f,0.0f,-20.0f));
 
     shader.use();
-    //view = processInput(window);
+    shader.setMat4("projection", proj);
     shader.setMat4("view", view);
-    shader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
+    shader.setVec3("objectColor", 0.8f, 0.07f, 0.7f);
     shader.setVec3("lightColor",  1.0f, 1.0f, 1.0f);
-
+    shader.setVec3("lightPos",  lightPos);
 
     glClearColor(0.0f,0.0f,0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -49,6 +44,8 @@ void Controller::mainLoop( void )
     model.draw(angle,shader);
 
     lightingShader.use();
+    lightingShader.setMat4("projection", proj);
+    lightingShader.setMat4("view", view);
     model.draw_lighting(lightingShader);
 
     glfwSwapBuffers(window);
--- source/mesh.cpp
+++ source/mesh.cpp
@@ -2,13 +2,14 @@
 
 void Mesh::draw_lighting( Shader shader)
 {
-  glm::mat4 model = glm::translate(glm::mat4(1.0f), location);
-  model = glm::rotate(model, glm::radians(10.0f), glm::vec3(2.0f,3.0f,0.0f));
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+  glm::mat4 model = glm::translate(glm::mat4(1.0f), lightPos);
+  model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
   shader.setMat4("model", model);
 
   // render the cube
   glBindVertexArray(lightVAO);
-  glDrawArrays(GL_TRIANGLES, 0, 36);
+  glDrawArrays(GL_TRIANGLES, 0, 1);
 }
 Mesh::Mesh(std::string filepath)
 {
@@ -32,6 +33,9 @@ void Mesh::setup()
   glEnableVertexAttribArray(0);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
 
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(sizeof(glm::vec3)));
+  glEnableVertexAttribArray(1);
+
   glBindBuffer(GL_ARRAY_BUFFER, 0);
   glBindVertexArray(0);
 }
@@ -39,47 +43,7 @@ void Mesh::setup()
 void Mesh::setupLighting()
 {
   float lightVertices[] = {
-    -0.1f, -0.1f, -0.1f,
-         0.1f, -0.1f, -0.1f,
-         0.1f,  0.1f, -0.1f,
-         0.1f,  0.1f, -0.1f,
-        -0.1f,  0.1f, -0.1f,
-        -0.1f, -0.1f, -0.1f,
-
-        -0.1f, -0.1f,  0.1f,
-         0.1f, -0.1f,  0.1f,
-         0.1f,  0.1f,  0.1f,
-         0.1f,  0.1f,  0.1f,
-        -0.1f,  0.1f,  0.1f,
-        -0.1f, -0.1f,  0.1f,
-
-        -0.1f,  0.1f,  0.1f,
-        -0.1f,  0.1f, -0.1f,
-        -0.1f, -0.1f, -0.1f,
-        -0.1f, -0.1f, -0.1f,
-        -0.1f, -0.1f,  0.1f,
-        -0.1f,  0.1f,  0.1f,
-
-         0.1f,  0.1f,  0.1f,
-         0.1f,  0.1f, -0.1f,
-         0.1f, -0.1f, -0.1f,
-         0.1f, -0.1f, -0.1f,
-         0.1f, -0.1f,  0.1f,
-         0.1f,  0.1f,  0.1f,
-
-        -0.1f, -0.1f, -0.1f,
-         0.1f, -0.1f, -0.1f,
-         0.1f, -0.1f,  0.1f,
-         0.1f, -0.1f,  0.1f,
-        -0.1f, -0.1f,  0.1f,
-        -0.1f, -0.1f, -0.1f,
-
-        -0.1f,  0.1f, -0.1f,
-         0.1f,  0.1f, -0.1f,
-         0.1f,  0.1f,  0.1f,
-         0.1f,  0.1f,  0.1f,
-        -0.1f,  0.1f,  0.1f,
-        -0.1f,  0.1f, -0.1f
+    -0.1f, -0.1f, -0.1f
     };
   glGenVertexArrays(1, &lightVAO);
   glGenBuffers(1, &lightVBO);
