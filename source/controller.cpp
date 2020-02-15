226d2983fc983ae15f1b2044ce53f79359ef7ca6
Kaustubh Nair <kaustubh.nair108@gmail.com>
2020-02-15 13:04:45 +0530

    nothing makes sense anymore

--- source/controller.cpp
+++ source/controller.cpp
@@ -13,7 +13,7 @@ void Controller::mainLoop( void )
   Shader shader("source/shaders/shader.vs", "source/shaders/shader.fs");
   Shader lightingShader("source/shaders/lighting_shader.vs", "source/shaders/lighting_shader.fs");
 
-  std::vector<std::string> filepaths = { "data/street_lamp.ply" };
+  std::vector<std::string> filepaths = { "data/beethoven.ply" };
   std::vector<glm::vec3> meshPos = {glm::vec3(0.0f,0.0f,0.0f)};
   //std::vector<std::string> filepaths = {"data/beethoven.ply", "data/shark.ply", "data/apple.ply" , "data/big_spider.ply"};
   //std::vector<glm::vec3> meshPos = {glm::vec3(-2.0,-2.0,-12.0), glm::vec3(5.0,0.0,-1.0), glm::vec3(5.0, -4.0, -1.0), glm::vec3(-5.0, 0.0, -1.0)};
@@ -31,12 +31,12 @@ void Controller::mainLoop( void )
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glm::mat4 proj = glm::perspective(glm::radians(45.0f), (float)800/(float)600, 0.1f, 100.0f);
-    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f,0.0f,-10.0f));
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f,0.0f,-15.0f));
 
     shader.use();
     shader.setMat4("projection", proj);
     shader.setMat4("view", view);
-    shader.setVec3("objectColor", 0.7f, 0.3f, 1.0f);
+    shader.setVec3("objectColor", 0.7f, 0.5f, 1.0f);
     shader.setVec3("lightColor",  1.0f, 1.0f, 1.0f);
     shader.setVec3("lightPos",  0.0f, 0.0f ,0.0f);
 
