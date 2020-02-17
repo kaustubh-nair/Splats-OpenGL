9da56c250d036756d0d5580b3329f9ebfdeac3f9
Kaustubh Nair <kaustubh.nair108@gmail.com>
2020-02-17 14:01:59 +0530

    COOL SPLATS NUMBER FOUR

--- source/mesh.cpp
+++ source/mesh.cpp
@@ -112,12 +112,12 @@ void Mesh::computeInCirleVertices()
 {
   std::vector<InCircle>::iterator inCircle;
 
-  int num_segments = 6;
+  int num_segments = 2;
 
   Vertex v;
   for(inCircle = inCircles.begin(); inCircle < inCircles.end(); inCircle++)
   {
-    int k = 1;
+    int k = 7;
     float radius = k * inCircle->radius;
     glm::vec3 v1 = inCircle->center;
     glm::vec3 v2 = v1 + (radius * normalize(glm::vec3(v.normal.z, 0, - v.normal.x)));
@@ -129,15 +129,18 @@ void Mesh::computeInCirleVertices()
     float angle = 360.0f/num_segments;
     for(int i = 1; i < num_segments; i++)
     {
-      v.position = v1;
-      inCircleVertices.push_back(v);
       glm::mat4 rotationMat = glm::rotate(glm::mat4(1.0f), glm::radians(angle*i), glm::normalize(inCircle->normal));
       glm::vec3 v3 = glm::vec3(rotationMat * glm::vec4(v2-v1,1.0f)) + v1;
+
+      v.position = v1;
+      inCircleVertices.push_back(v);
       v.position = v3;
       inCircleVertices.push_back(v);
       inCircleVertices.push_back(v);
     }
     v.position = v1;
+    inCircleVertices.push_back(v);
+    v.position = v2;
     inCircleVertices.push_back(v);
   }
 }
